b9ef7273b1c71eb7f5a28cdc9d316ef97dab22d2
JoloTheGr8 <joshuahonrado25@gmail.com>
2023-08-16 13:56:31 +0800

    feat: added header, toggleMenu and toggle light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
@@ -11,6 +12,7 @@ import Footer from './components/footer/Footer'
 function App() {
   return (
     <main className="main">
+      <Header />
       <Home />
       <Services />
       <Skills />
